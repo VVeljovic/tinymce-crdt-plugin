9769cb56452269f09154dc11920d97afe156747c
VVeljovic <veljkoveljovic13@gmail.com>
2026-09-01 15:44:03 +0200

    starting with writing document

---
+++ CrdtCore/CrdtFormatting.cs
@@ -0,0 +1,13 @@
+﻿namespace CrdtCore
+{
+    public sealed record CrdtFormatting
+    {
+        public CrdtId FormattingId { get; set; }
+
+        public CrdtId Start { get; set; }
+
+        public CrdtId End { get; set; }
+
+        public TextAttributes Attributes { get; set; }
+    }
+}
---
+++ CrdtCore/TextAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace CrdtCore
+{
+    public sealed record TextAttributes
+    {
+        public bool Bold { get; set; }
+
+        public bool Italic { get; set; }
+
+        public bool Underline { get; set; }
+    }
+}
